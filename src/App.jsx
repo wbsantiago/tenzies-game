51b3b14d6f70fca7bae0a542141e1547a2d01ae3
wbsantiago <will.jimmy27@hotmail.com>
2022-10-13 19:11:53 -0300

    styled all component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Die from './components/Die.jsx'
+
 
 function App() {
 
   return (
     <main className="App">
-      
+      <h1 className='tenzie-title'>Tenzies</h1>
+      <p className='tenzie-explanation'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <Die />
+      <button className='tenzie-btn'>Roll</button>
     </main>
   )
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,18 @@
+import './die.css'
+
+export default function Die(props) {
+    return (
+        <div className='container'>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+            <div className="die-dices">6</div>
+        </div>
+    )
+}
