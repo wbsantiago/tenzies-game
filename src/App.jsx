8320bc55b15d1715dc9f365a56e2243047be1f17
wbsantiago <will.jimmy27@hotmail.com>
2022-10-13 21:46:50 -0300

    added random number function and start generate all 10 dice with for condition

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,31 @@ import Die from './components/Die.jsx'
 
 function App() {
 
+
+function randomDieValue() {
+  return Math.ceil(Math.random() * 6)
+}
+
+function allNewDice() {
+  const newArray = []
+  for(let i = 0; i < 10 ; i++) {
+    const newDie = {
+      value: randomDieValue(),
+      id: i + 1
+    }
+    newArray.push(newDie)
+  }
+  return newArray
+}
+
+console.log(allNewDice())
+
   return (
     <main className="App">
       <h1 className='tenzie-title'>Tenzies</h1>
       <p className='tenzie-explanation'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <Die />
+      <div className='container'>
+      </div>
       <button className='tenzie-btn'>Roll</button>
     </main>
   )
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,17 +2,8 @@ import './die.css'
 
 export default function Die(props) {
     return (
-        <div className='container'>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
-            <div className="die-dices">6</div>
+        <div >
+            <h2 className="die-dices">{props.value}</h2>
         </div>
     )
 }
