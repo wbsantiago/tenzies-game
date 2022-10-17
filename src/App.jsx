078f5a5eb454deee71dc78e1110c64161f79131d
wbsantiago <will.jimmy27@hotmail.com>
2022-10-17 15:25:33 -0300

    import confetti effect and conditional render at button when tenzies state is true

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import './App.css'
 import Die from './components/Die'
 import React from 'react'
 import { nanoid } from 'nanoid'
+import { useEffect } from 'react'
+import Confetti from 'react-confetti'
+
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice())
-  const [tenzies, setTenzies] = React.useState()
+  const [tenzies, setTenzies] = React.useState(false)
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)  
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+    }
+  }, [dice])
 
   function randomDieValue() {
     return Math.ceil(Math.random() * 6)
@@ -41,23 +53,34 @@ function App() {
   }
 
   function holdDice(id) {
-    setDice(prevDice => prevDice.map(die => {
-      return  die.id === id ?
-        {...die, isHeld: !die.isHeld} :
-        die
-    }))
+    if (tenzies) {
+      allNewDice()
+      tenzies === false
+    } else {
+      setDice(prevDice => prevDice.map(die => {
+        return  die.id === id ?
+          {...die, isHeld: !die.isHeld} :
+          die
+      }))
+    }
   }
 
   const diceElements = dice.map(die => <Die key={die.id} {...die} held={die.isHeld} holdDice={() => holdDice(die.id)} />)
 
   return (
     <main className="App">
+      { tenzies && <Confetti />}
       <h1 className='tenzie-title'>Tenzies</h1>
       <p className='tenzie-explanation'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='container'>
         {diceElements}
       </div>
-      <button className='tenzie-btn' onClick={rollUnheldDice}>Roll</button>
+      <button 
+        className='tenzie-btn' 
+        onClick={rollUnheldDice}
+      >
+        { tenzies ? "New Game" : "Roll"}
+      </button>
     </main>
   )
 }
